4d3155410f7a93753d8321a9aae0aa51751a2445
orzamir87 <orzamir@outlook.com>
2016-07-18 08:46:26 -0400

    Assembly Structure

--- DataStructures/AssemblyByte.c
+++ DataStructures/AssemblyByte.c
@@ -6,4 +6,10 @@
 
 struct AssemblyByte {
 
+    char bitarray[15];
+
+    //Byte type: command or addressing
+
+    //Command: new functions ("class") will help decide on addressing types, A.R.E etc...
+    //Addressing: type (forward, register, bypass)
 };
