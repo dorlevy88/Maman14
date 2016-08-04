98798eeb050ee62c8fa7e6e38d7c6def7ce51c44
dorl <dorlevy88@gmail.com>
2016-08-03 22:34:00 -0400

    Working for the first time!

--- Output/OutputFiles.c
+++ Output/OutputFiles.c
@@ -2,34 +2,79 @@
 // Created by Or Zamir on 7/31/16.
 //
 
+#include <stdlib.h>
+#include <printf.h>
 #include "OutputFiles.h"
 
 //Trim int to 16Bit for writing
 #define to16bit(n) (n)>=0? n : ((n)^-65536)
 
 
-bool writeEntOutpuFile(SymbolsTable* table) {
+
+
+
+bool writeEntOutputFile(SymbolsTable *table) {
     //TODO: implement
 }
 
-bool writeExtOutpuFile(SymbolsTable* table) {
+bool writeExtOutputFile(SymbolsTable *table) {
     //TODO: implement
 }
 
-bool writeObOutpuFile(SymbolsTable* table) {
-    //TODO: implement
+bool writeObOutputFile(AssemblyBytes* code, AssemblyBytes* data, int ic) {
+    for (int i = 0; i < code->size; ++i) {
+        printf("");
+    }
 }
 
-char* transtaleToSpecial8Base(int threeBytes[3]) {
+char translateToSpecial8Base(int base10) {
+    switch (base10) {
+        case 0:
+            return '!';
+        case 1:
+            return '@';
+        case 2:
+            return '#';
+        case 3:
+            return '$';
+        case 4:
+            return '%';
+        case 5:
+            return '^';
+        case 6:
+            return '&';
+        case 7:
+            return '*';
+        default:
+            //bug
+            return NULL;
+    }
+}
 
+char* transtaleCommandToSpecial8Base(int byte) {
+    char* response = (char*) malloc(sizeof(char) * 5);
+    for (int i = 0; i < 5; ++i) {
+        int num = byte & 0b111; //Get 3 right most bits
+        response[i] = translateToSpecial8Base(num);
+    }
+    return response;
+}
+
+char* transtaleAddressToSpecial8Base(int address) {
+    char* response = (char*) malloc(sizeof(char) * 3);
+    for (int i = 1; i <= 3 ; ++i) { //3 because it could be up to 1000
+        int num = address % (i * 10);
+        response[i] = translateToSpecial8Base(num);
+    }
+    return response;
 }
 
 bool WriteAllOutpuFiles(AssemblyStructure* assembly) {
     //TODO: implement
 
-    writeEntOutpuFile(assembly->symbolsTable);
+    writeEntOutputFile(assembly->symbolsTable);
 
-    writeExtOutpuFile(assembly->symbolsTable);
+    writeExtOutputFile(assembly->symbolsTable);
 
-    writeObOutpuFile(assembly->symbolsTable);
+    writeObOutputFile(assembly->symbolsTable);
 }
